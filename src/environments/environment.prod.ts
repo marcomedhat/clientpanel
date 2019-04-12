2a920ab00c917b8fc97002f62b71b76424892af8
Marco-Medhat <eng.marcomedhat@gmail.com>
2019-04-12 19:39:49 +0200

    adding configuration to environmet.ts & environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  	apiKey: "AIzaSyAq4V_pGDRteK5X-U8bGs3wXIovkZ7vmvU",
+    authDomain: "clientpanelprod-c40e5.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-c40e5.firebaseio.com",
+    projectId: "clientpanelprod-c40e5",
+    storageBucket: "clientpanelprod-c40e5.appspot.com",
+    messagingSenderId: "657653980141"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  	apiKey: "AIzaSyAq4V_pGDRteK5X-U8bGs3wXIovkZ7vmvU",
+    authDomain: "clientpanelprod-c40e5.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-c40e5.firebaseio.com",
+    projectId: "clientpanelprod-c40e5",
+    storageBucket: "clientpanelprod-c40e5.appspot.com",
+    messagingSenderId: "657653980141"
+  }
 };
 
 /*
