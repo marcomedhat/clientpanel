2d5ca7919ddffb38e5805a5070ea32fdba33babb
Marco-Medhat <eng.marcomedhat@gmail.com>
2019-04-19 16:44:06 +0200

    creating client service

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, 
+	AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs/Observable';
 
+import { Client } from '../models/Client';
+}
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
+	clientsCollection: AngularFirestoreCollection<Client>;
+	clientDoc: AngularFirestoreDocument<Client>;
+	clients: Observable<Client[]>;
+	client: Observable<Client>;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {
+  	this.clientsCollection = this.afs.collection('clients',
+  	 ref => ref.orderBy('lastName', 'asc'));
+  }
+
+  getClients(): Observable<Client[]> {
+		// Get clients with the id
+		this.clients = this.clientsCollection.snapshotChanges()
+		.map(changes => {
+			return changes.map(action => {
+				const data = action.payload.doc.data() as Client;
+				data.id = action.payload.doc.id;
+				return data;
+			});
+		});
+
+		return this.clients;
+  }
 }
